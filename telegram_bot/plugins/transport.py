4c0ceb9cba2fffb6f6de3b5d22da2caf819f5511
Alan <bunburya@tcd.ie>
2018-05-13 20:50:27 +0000

    added initial draft transport plugin

---
+++ telegram_bot/plugins/transport.py
@@ -0,0 +1,45 @@
+from plugin_handler import Plugin
+
+from dublin_transport import DublinTransportData
+
+class TransportPlugin(Plugin):
+    
+    def __init__(self):
+        self.handlers = {'text': self.handle_text}
+        self.dtd = DublinTransportData()
+            
+    def handle_text(self, msg):
+        chat_id = msg['chat']['id']
+        text = msg['text'].lower()
+        if text == 'transport':
+            self.send_transport_data(chat_id)
+
+    def send_transport_data(self, chat_id):
+        self.send_luas_data(chat_id)
+        self.send_bus_data(chat_id)
+
+    def send_bus_data(self, chat_id):
+        self.bot.bot.sendMessage(chat_id, 'BUS:')
+        try:
+            data = self.dtd.get_rtpi_data('BUS')
+        except Exception as e:
+            self.bot.bot.sendMessage(chat_id, 'Error fetching data.')
+            raise e
+        for bus in data['Inbound']:
+            route, dest, due = bus['route'], bus['destination'], bus['duetime']
+            self.bot.bot.sendMessage(chat_id, '{} to {}, due in {}.'.format(route, dest, due))
+        if not data['Inbound']:
+            self.bot.bot.sendMessage(chat_id, 'No buses due.')
+
+    def send_luas_data(self, chat_id):
+        self.bot.bot.sendMessage(chat_id, 'LUAS:')
+        try:
+            data = self.dtd.get_rtpi_data('LUAS')
+        except Exception as e:
+            self.bot.bot.sendMessage(chat_id, 'Error fetching data.')
+            raise e
+        for luas in data['Inbound']:
+            dest, due = luas['destination'].lstrip('LUAS '), luas['duetime']
+            self.bot.bot.sendMessage(chat_id, 'To {}, due in {}.'.format(dest, due))
+        if not data['Inbound']:
+            self.bot.bot.sendMessage(chat_id, 'No Luas due.')
